98e6682e50049283ab95679be678c5a86a2d16d1
nicolaserny <nicolas.erny@xcomponent.com>
2020-07-22 17:10:00 +0200

    Fix layout margins

--- src/components/layout.jsx
+++ src/components/layout.jsx
@@ -3,14 +3,16 @@ import NavBar from "./navBar";
 import Footer from "./footer";
 
 const Layout = ({ className, children }) => (
-  <div
-    className={`grid grid-rows-layout max-w-screen-xl my-12 mx-auto ${className}`}
-  >
-    <header className="w-full">
-      <NavBar />
-    </header>
-    <main>{children}</main>
-    <Footer />
+  <div className="ml-20 mr-20">
+    <div
+      className={`grid grid-rows-layout max-w-screen-xl my-12 mx-auto ${className}`}
+    >
+      <header className="w-full">
+        <NavBar />
+      </header>
+      <main>{children}</main>
+      <Footer />
+    </div>
   </div>
 );
 
