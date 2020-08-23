472c0ffa754db1cb6e0833c682f0d64b31c719f3
nicolaserny <nicolas.erny@xcomponent.com>
2020-08-23 17:07:34 +0200

    feat(About page): improve bullet list design

--- src/pages/about.js
+++ src/pages/about.js
@@ -16,6 +16,23 @@ export const query = graphql`
   }
 `;
 
+const Customli = ({ children }) => (
+  <li className="mb-1 last:mb-0">
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      viewBox="0 0 100 125"
+      height="1.4em"
+      className="inline-block mr-4  text-purple-600"
+    >
+      <path
+        fill="currentColor"
+        d="M50 3.75C24.498 3.75 3.75 24.498 3.75 50S24.498 96.25 50 96.25 96.25 75.502 96.25 50 75.502 3.75 50 3.75zm25.636 36.827L46.098 70.115a4.866 4.866 0 01-3.416 1.415v2l-.043-2a4.855 4.855 0 01-3.433-1.476L24.304 54.608a4.835 4.835 0 01.122-6.83 4.803 4.803 0 013.353-1.354c1.323 0 2.558.524 3.477 1.476l11.488 11.907 26.062-26.061a4.8 4.8 0 013.416-1.415c1.29 0 2.503.502 3.415 1.414a4.838 4.838 0 01-.001 6.832z"
+      />
+    </svg>
+    {children}
+  </li>
+);
+
 const About = ({ data }) => (
   <Layout>
     <SEO title="About" />
@@ -31,19 +48,19 @@ const About = ({ data }) => (
         </p>
         <div className="mb-3 md:mb-5 lg:mb-8">
           <span className="font-medium">Mes influences actuelles :</span>
-          <ol className="list-disc list-inside mt-1 leading-loose">
-            <li>hatha yoga, vinyasa yoga</li>
-            <li>CSV (core strength vinyasa) yoga et yoga shred</li>
-            <li>LYT yoga method</li>
-            <li>Primal vinyasa yoga</li>
-            <li>Budokon yoga</li>
-            <li>Animal flow</li>
+          <ol className="mt-2 leading-loose">
+            <Customli>hatha yoga, vinyasa yoga</Customli>
+            <Customli>CSV (core strength vinyasa) yoga et yoga shred</Customli>
+            <Customli>LYT yoga method</Customli>
+            <Customli>Primal vinyasa yoga</Customli>
+            <Customli>Budokon yoga</Customli>
+            <Customli>Animal flow</Customli>
           </ol>
         </div>
         <div className="mb-3 md:mb-5 lg:mb-8">
           <span className="font-medium">Mes formations :</span>
-          <ol className="list-disc list-inside mt-1 leading-loose">
-            <li>
+          <ol className="mt-2 leading-loose">
+            <Customli>
               Hatha yoga:{" "}
               <a
                 className="text-purple-600 hover:text-purple-800 font-semibold underline"
@@ -53,8 +70,8 @@ const About = ({ data }) => (
               >
                 EFY, école française du yoga
               </a>
-            </li>
-            <li>
+            </Customli>
+            <Customli>
               Yoga alchimique, avec{" "}
               <a
                 className="text-purple-600 hover:text-purple-800 font-semibold underline"
@@ -64,8 +81,8 @@ const About = ({ data }) => (
               >
                 Mira Jamadi
               </a>
-            </li>
-            <li>
+            </Customli>
+            <Customli>
               <a
                 className="text-purple-600 hover:text-purple-800 font-semibold underline"
                 href="https://www.running-yogis.com"
@@ -74,8 +91,8 @@ const About = ({ data }) => (
               >
                 Running yoga
               </a>
-            </li>
-            <li>
+            </Customli>
+            <Customli>
               En cours de formation :{" "}
               <a
                 className="text-purple-600 hover:text-purple-800 font-semibold underline"
@@ -85,7 +102,7 @@ const About = ({ data }) => (
               >
                 LYT yoga method
               </a>
-            </li>
+            </Customli>
           </ol>
         </div>
       </div>
