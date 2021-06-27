416155d1dca9399f34c59ea34ec026c61901f4fb
nicolaserny <nicolas.erny@xcomponent.com>
2021-06-27 16:47:18 +0200

    Update announcement (summer courses)

--- src/components/announcement.js
+++ src/components/announcement.js
@@ -4,10 +4,10 @@ import { Link } from "gatsby";
 const Announcement = () => (
   <div className="w-full flex justify-center p-4 text-white font-medium text-base bg-gradient-to-r from-purple-600 to-red-600">
     <p className="">
-      Découvrir les nouveaux cours de Yoga-balles à partir du 12 mars 2021.{" "}
+      Le programme des cours de cet été est en ligne.{" "}
       <Link
         className="text-white hover:no-underline font-medium underline focus:no-underline focus:outline-none focus:ring-1 focus:ring-white"
-        to="/yoga-balles/"
+        to="/schedule/"
       >
         En savoir plus...
       </Link>
