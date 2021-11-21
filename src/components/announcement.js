a7e56104c87350265311d0ec09de571306176cb1
nicolaserny <nicolas.erny@xcomponent.com>
2021-11-21 16:13:49 +0100

    Update announcement

--- src/components/announcement.js
+++ src/components/announcement.js
@@ -4,7 +4,7 @@ import { Link } from "gatsby";
 const Announcement = () => (
   <div className="w-full flex justify-center p-4 text-white font-medium text-base bg-gradient-to-r from-purple-600 to-red-600">
     <p className="">
-      Découvrez le programme des cours de Novembre.{" "}
+      Découvrez le programme des cours de Décembre.{" "}
       <Link
         className="text-white hover:no-underline font-medium underline focus:no-underline focus:outline-none focus:ring-1 focus:ring-white"
         to="/schedule/"
