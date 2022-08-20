a152a55199ead8a082ad5d738b1c50f7f20595ec
nicolaserny <nicolas.erny@xcomponent.com>
2022-08-20 18:08:10 +0200

    Netlify deployment

--- app/utils/misc.server.ts
+++ app/utils/misc.server.ts
@@ -1,16 +1,16 @@
 export function getSiteUrl() {
   const defaultUrl = "https://yogabydelphine.com";
-  // console.log("context", process.env.CONTEXT_ENV);
-  // if (process.env.CONTEXT === "preview") {
-  //   const url = process.env.DEPLOY_PRIME_URL || process.env.URL || defaultUrl;
-  //   console.log("url", url);
-  //   return url;
-  // }
-  // if (process.env.CONTEXT === "dev") {
-  //   return "http://localhost:3000";
-  // }
-  // const url = process.env.URL || defaultUrl;
-  const url = process.env.DEPLOY_PRIME_URL || process.env.URL || defaultUrl;
+  const context = process.env.CONTEXT || "production";
+  console.log("context", context);
+  if (["deploy-preview", "branch-deploy"].includes(context)) {
+    const url = process.env.DEPLOY_PRIME_URL || defaultUrl;
+    console.log("url", url);
+    return url;
+  }
+  if (context === "dev") {
+    return "http://localhost:3000";
+  }
+  const url = process.env.URL || defaultUrl;
   console.log("url", url);
   return url;
 }
