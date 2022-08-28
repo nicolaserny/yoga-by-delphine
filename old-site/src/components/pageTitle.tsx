a7de30529a49b95c593dfa392c0deee3c8333999
nicolaserny <nicolas.erny@xcomponent.com>
2022-08-28 18:18:50 +0200

    Remove old gatsby site

--- old-site/src/components/pageTitle.tsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-const PageTitle: React.FC<{}> = ({ children }) => (
-  <h1 className="mt-3 lg:mt-12 text-gray-800 text-xl lg:text-2xl font-semibold">
-    {children}
-  </h1>
-);
-
-export default PageTitle;
