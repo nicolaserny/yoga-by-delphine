5615c175786c35ca3ec55e90fb40e832355003ca
nicolaserny <nicolas.erny@xcomponent.com>
2022-10-23 14:33:49 +0200

    Fix bundle

--- remix.config.js
+++ remix.config.js
@@ -3,6 +3,13 @@ module.exports = {
   serverBuildTarget: "netlify",
   server: "./server.js",
   ignoredRouteFiles: ["**/.*"],
+  serverDependenciesToBundle: [
+    "@remix-run/web-blob",
+    "@remix-run/web-fetch",
+    "@remix-run/web-form-data",
+    "@remix-run/web-stream",
+    "data-uri-to-buffer",
+  ],
   // appDirectory: "app",
   // assetsBuildDirectory: "public/build",
   // serverBuildPath: ".netlify/functions-internal/server.js",
