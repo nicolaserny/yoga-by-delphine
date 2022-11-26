d948a4e44945d082f45368d54d05eb4635860a6a
nicolaserny <nicolas.erny@xcomponent.com>
2022-11-26 16:22:11 +0100

    Update announcement

--- app/components/announcement.tsx
+++ app/components/announcement.tsx
@@ -4,7 +4,7 @@ import Button from "./button";
 const Announcement = () => (
   <div className="flex w-full justify-center bg-gradient-to-r from-purple-600 to-red-600 p-4 text-base font-medium text-white">
     <p className="">
-      Le planning de novembre est en ligne !{" "}
+      Le planning de décembre est en ligne !{" "}
       <Button
         as={Link}
         variant="link"
