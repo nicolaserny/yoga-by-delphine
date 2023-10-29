727d69af718070a43bb1e5d1f6b1580e7bae9353
nicolaserny <nicolas.erny@xcomponent.com>
2023-10-29 14:43:20 +0100

    Remove annoucement from the homepage

--- app/components/layout.tsx
+++ app/components/layout.tsx
@@ -1,13 +1,11 @@
 import React from "react";
 import { NavBar } from "./navBar";
 import Footer from "./footer";
-import Announcement from "./announcement";
 
 const Layout: React.FC<{ children: React.ReactNode }> = ({ children }) => (
   <div>
-    <Announcement />
     <div
-      className={`min-h-realsm lg:min-h-real grid grid-rows-layout  my-6 lg:my-12 mx-8 lg:mx-20 xl:mx-0`}
+      className={`mx-8 my-6 grid min-h-realsm  grid-rows-layout lg:mx-20 lg:my-12 lg:min-h-real xl:mx-0`}
     >
       <header className="width-constraints">
         <NavBar />
