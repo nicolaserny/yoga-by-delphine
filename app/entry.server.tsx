e29216cc289bc17ae9ca3e39e5e69354b40d1386
nicolaserny <nicolas.erny@xcomponent.com>
2023-12-14 17:41:50 +0100

    Update node/packages + fix netlify bug in dev mode (use the previous hack)

--- app/entry.server.tsx
+++ app/entry.server.tsx
@@ -1,34 +1,8 @@
-import type { AppLoadContext, EntryContext } from "@netlify/remix-runtime";
-import { RemixServer } from "@remix-run/react";
-import isbot from "isbot";
-import { renderToReadableStream } from "react-dom/server";
-
-export default async function handleRequest(
-  request: Request,
-  responseStatusCode: number,
-  responseHeaders: Headers,
-  remixContext: EntryContext,
-  loadContext: AppLoadContext,
-) {
-  const body = await renderToReadableStream(
-    <RemixServer context={remixContext} url={request.url} />,
-    {
-      signal: request.signal,
-      onError(error: unknown) {
-        // Log streaming rendering errors from inside the shell
-        console.error(error);
-        responseStatusCode = 500;
-      },
-    },
-  );
-
-  if (isbot(request.headers.get("user-agent"))) {
-    await body.allReady;
-  }
-
-  responseHeaders.set("Content-Type", "text/html");
-  return new Response(body, {
-    headers: responseHeaders,
-    status: responseStatusCode,
-  });
+let handleRequestImpl;
+if (process.env.NODE_ENV === "development") {
+  handleRequestImpl = require("./entry.dev.server.tsx").default;
+} else {
+  handleRequestImpl = require("./entry.prod.server.tsx").default;
 }
+
+export default handleRequestImpl;
--- remix.config.js
+++ remix.config.js
@@ -1,9 +1,13 @@
 const { config } = require("@netlify/remix-edge-adapter");
+const baseConfig =
+  process.env.NODE_ENV === "production"
+    ? config
+    : { ignoredRouteFiles: ["**/.*"], serverModuleFormat: "cjs" };
 
 /**
  * @type {import('@remix-run/dev').AppConfig}
  */
 module.exports = {
-  ...config,
+  ...baseConfig,
   tailwind: true,
 };
