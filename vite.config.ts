a2a3c28b391570258b9b0a6ea1921da0d3632c75
nicolaserny <nicolaserny@gmail.com>
2025-02-03 16:17:55 +0100

    Use tailwind vite plugin

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,9 @@
 import { netlifyPlugin } from "@netlify/remix-edge-adapter/plugin";
 import { vitePlugin as remix } from "@remix-run/dev";
+import tailwindcss from "@tailwindcss/vite";
 import { defineConfig } from "vite";
 import tsconfigPaths from "vite-tsconfig-paths";
 
 export default defineConfig({
-  plugins: [remix(), netlifyPlugin(), tsconfigPaths()],
+  plugins: [tailwindcss(), remix(), netlifyPlugin(), tsconfigPaths()],
 });
