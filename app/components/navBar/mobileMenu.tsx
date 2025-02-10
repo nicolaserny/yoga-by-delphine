7a06386bd7bf87fdbf996b6622d906e321f7b370
nicolaserny <nicolaserny@gmail.com>
2025-02-10 13:42:13 +0100

    Migrate from reach-ui to headless-ui (mobile navbar)

--- app/components/navBar/mobileMenu.tsx
+++ app/components/navBar/mobileMenu.tsx
@@ -1,4 +1,4 @@
-import { DialogContent, DialogOverlay } from "@reach/dialog";
+import { Dialog, DialogPanel } from "@headlessui/react";
 import { Link } from "@remix-run/react";
 import React from "react";
 import Button from "../button";
@@ -23,79 +23,77 @@ const MobileMenu: React.FC<{ isOpen: boolean; onDismiss: () => void }> = ({
   onDismiss,
 }) => {
   return (
-    <DialogOverlay
-      className="fixed inset-0 bg-white"
-      isOpen={isOpen}
-      onDismiss={onDismiss}
-    >
-      <DialogContent aria-label="Menu">
-        <button
-          className={`fixed top-8 right-6 p-3 focus:outline-hidden focus-visible:ring-2 focus-visible:ring-purple-500 focus-visible:ring-offset-2`}
-          onClick={onDismiss}
-        >
-          <svg
-            width="35"
-            height="35"
-            viewBox="0 0 46 46"
-            fill="none"
-            xmlns="http://www.w3.org/2000/svg"
+    <Dialog open={isOpen} onClose={onDismiss} className="z-50">
+      <div className="fixed inset-0 bg-white">
+        <DialogPanel aria-label="Menu">
+          <button
+            className={`fixed top-8 right-6 p-3 focus:outline-hidden focus-visible:ring-2 focus-visible:ring-purple-500 focus-visible:ring-offset-2`}
+            onClick={onDismiss}
           >
-            <path
-              d="M24.848 23.056L36.112 34.32l-1.792 1.792-11.264-11.264-11.264 11.264L10 34.32l11.264-11.264L10 11.792 11.792 10l11.264 11.264L34.32 10l1.792 1.792-11.264 11.264z"
-              className="fill-gray-800"
-            />
-          </svg>
-          <span className="sr-only">Menu</span>
-        </button>
-        <ul className={`block list-none pt-24`}>
-          <li>
-            <NavLink to="/" onDismiss={onDismiss}>
-              Accueil
-            </NavLink>
-          </li>
-          <li>
-            <NavLink to="/schedule/" onDismiss={onDismiss}>
-              Programme
-            </NavLink>
-          </li>
-          <li>
-            <NavLink to="/gift-cards/" onDismiss={onDismiss}>
-              Cartes-cadeaux
-            </NavLink>
-          </li>
-          <li>
-            <NavLink to="/videos/" onDismiss={onDismiss}>
-              Vidéos
-            </NavLink>
-          </li>
-          <li>
-            <NavLink to="/about/" onDismiss={onDismiss}>
-              A propos
-            </NavLink>
-          </li>
-          <li>
-            <NavLink to="/contact/" onDismiss={onDismiss}>
-              Contact
-            </NavLink>
-          </li>
-          <li>
-            <Button
-              as={Link}
-              variant="outline"
-              colorScheme="red"
-              to="/schedule/"
-              onClick={onDismiss}
-              className="mt-3 ml-12"
-              responsive={false}
-              size="xlarge"
-              prefetch="intent"
+            <svg
+              width="35"
+              height="35"
+              viewBox="0 0 46 46"
+              fill="none"
+              xmlns="http://www.w3.org/2000/svg"
             >
-              Réserver
-            </Button>
-          </li>
-        </ul>
-      </DialogContent>
-    </DialogOverlay>
+              <path
+                d="M24.848 23.056L36.112 34.32l-1.792 1.792-11.264-11.264-11.264 11.264L10 34.32l11.264-11.264L10 11.792 11.792 10l11.264 11.264L34.32 10l1.792 1.792-11.264 11.264z"
+                className="fill-gray-800"
+              />
+            </svg>
+            <span className="sr-only">Menu</span>
+          </button>
+          <ul className={`block list-none pt-24`}>
+            <li>
+              <NavLink to="/" onDismiss={onDismiss}>
+                Accueil
+              </NavLink>
+            </li>
+            <li>
+              <NavLink to="/schedule/" onDismiss={onDismiss}>
+                Programme
+              </NavLink>
+            </li>
+            <li>
+              <NavLink to="/gift-cards/" onDismiss={onDismiss}>
+                Cartes-cadeaux
+              </NavLink>
+            </li>
+            <li>
+              <NavLink to="/videos/" onDismiss={onDismiss}>
+                Vidéos
+              </NavLink>
+            </li>
+            <li>
+              <NavLink to="/about/" onDismiss={onDismiss}>
+                A propos
+              </NavLink>
+            </li>
+            <li>
+              <NavLink to="/contact/" onDismiss={onDismiss}>
+                Contact
+              </NavLink>
+            </li>
+            <li>
+              <Button
+                as={Link}
+                variant="outline"
+                colorScheme="red"
+                to="/schedule/"
+                onClick={onDismiss}
+                className="mt-3 ml-12"
+                responsive={false}
+                size="xlarge"
+                prefetch="intent"
+              >
+                Réserver
+              </Button>
+            </li>
+          </ul>
+        </DialogPanel>
+      </div>
+    </Dialog>
   );
 };
 
