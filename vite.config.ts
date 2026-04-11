6db39c58b5bd526278a89f2185476e8abb14f603
Nicolas ERNY <nicolaserny@gmail.com>
2026-04-11 18:06:59 +0200

    chore: upgrade to vite 8 + minor updates (#70)
    * chore: upgrade to vite 8 + minor updates
    
    * chore: upgrade typescript

--- vite.config.ts
+++ vite.config.ts
@@ -3,14 +3,15 @@ import netlifyPlugin from "@netlify/vite-plugin-react-router";
 import { reactRouter } from "@react-router/dev/vite";
 import tailwindcss from "@tailwindcss/vite";
 import { defineConfig } from "vite";
-import tsconfigPaths from "vite-tsconfig-paths";
 
 export default defineConfig({
   plugins: [
     tailwindcss(),
     reactRouter(),
     netlifyPlugin({ edge: true }),
-    tsconfigPaths(),
     netlify(),
   ],
+  resolve: {
+    tsconfigPaths: true,
+  },
 });
